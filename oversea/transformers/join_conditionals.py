3c35a6c125464dc9748ff64a11dca8f2dcdd3a3e
michal bargiel <michal5bargiel@gmail.com>
2021-07-16 14:00:51 +0200

    💩 Try to process conditionals.

--- oversea/transformers/join_conditionals.py
+++ oversea/transformers/join_conditionals.py
@@ -1,9 +1,11 @@
+import pprint
 from typing import List
 
 from spacy.tokens import Span
 
 
-def join_conditionals(
-    sentences: List[Span]
-) -> List[Span]:
-    pass
+def join_conditionals(sentences: List[Span]) -> List[Span]:
+    pprint.pprint(sentences)
+    fourth = sentences[3]
+    print(dir(sentences[3][0]))
+    return sentences
--- oversea/transformers/test_transformers/test_join_conditionals.py
+++ oversea/transformers/test_transformers/test_join_conditionals.py
@@ -1,8 +1,34 @@
+import spacy.tokens
+
 from oversea.transformers.join_conditionals import join_conditionals
 
 
-def test_let_s_try_this(
+def test_is_endofunctor(
     kraken_sentences,
 ):
-    print(kraken_sentences)
-    join_conditionals(kraken_sentences)
+    transformed = join_conditionals(kraken_sentences)
+
+    assert isinstance(transformed, list)
+    for sentence in transformed:
+        isinstance(sentence, spacy.tokens.Span)
+
+
+def test_first_sentence_is_a_fluff(kraken_sentences):
+    action_keyword = "rzuć"
+    [first, *transformed] = join_conditionals(kraken_sentences)
+
+    assert action_keyword not in first.text.lower()
+
+
+def test_second_sentence_has_action_keyword(kraken_sentences):
+    action_keyword = "rzuć"
+    [first, second, *transformed] = join_conditionals(kraken_sentences)
+
+    assert action_keyword in second.text.lower()
+
+
+def test_every_next_starts_with_number(kraken_sentences):
+    [first, second, *transformed] = join_conditionals(kraken_sentences)
+
+    for sentence in transformed:
+        assert False
