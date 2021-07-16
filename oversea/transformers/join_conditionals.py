7dc44407c8ada1704099cb5b85574837f0d0fb12
michal bargiel <michal5bargiel@gmail.com>
2021-07-16 12:17:26 +0200

    ✨ Scripts for loading and saving data processed by spacy.

---
+++ oversea/transformers/join_conditionals.py
@@ -0,0 +1,9 @@
+from typing import List
+
+from spacy.tokens import Span
+
+
+def join_conditionals(
+    sentences: List[Span]
+) -> List[Span]:
+    pass
---
+++ oversea/transformers/test_transformers/test_join_conditionals.py
@@ -0,0 +1,8 @@
+from oversea.transformers.join_conditionals import join_conditionals
+
+
+def test_let_s_try_this(
+    kraken_sentences,
+):
+    print(kraken_sentences)
+    join_conditionals(kraken_sentences)
