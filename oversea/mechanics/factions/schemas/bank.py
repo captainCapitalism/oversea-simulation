c2589d25b38b9718d1af74cadeb30114a7725afb
michal bargiel <michal5bargiel@gmail.com>
2021-07-28 23:55:17 +0200

    ♻️ Make resources value objects.

--- oversea/mechanics/factions/schemas/bank.py
+++ oversea/mechanics/factions/schemas/bank.py
@@ -2,24 +2,23 @@ from oversea.mechanics.factions.schemas.base_resources import BaseResources
 
 
 class Bank(BaseResources):
-    pass
-
     def __add__(
         self,
         income: BaseResources,
     ) -> "Bank":
-        self.cash += income.cash
-        self.geist += income.geist
-        self.intrigue_cards += income.intrigue_cards
-        self.magic_cards += income.magic_cards
-        self.goal_cards += income.goal_cards
-        return self
+        return Bank(
+            cash=self.cash + income.cash,
+            geist=self.geist + income.geist,
+            intrigue_cards=self.intrigue_cards + income.intrigue_cards,
+            magic_cards=self.magic_cards + income.magic_cards,
+            goal_cards=self.goal_cards + income.goal_cards,
+        )
 
     def __sub__(self, cost: BaseResources):
-
-        self.cash -= cost.cash
-        self.geist -= cost.geist
-        self.intrigue_cards -= cost.intrigue_cards
-        self.magic_cards -= cost.magic_cards
-        self.goal_cards -= cost.goal_cards
-        return self
+        return Bank(
+            cash=self.cash - cost.cash,
+            geist=self.geist - cost.geist,
+            intrigue_cards=self.intrigue_cards - cost.intrigue_cards,
+            magic_cards=self.magic_cards - cost.magic_cards,
+            goal_cards=self.goal_cards - cost.goal_cards,
+        )
--- oversea/mechanics/factions/schemas/base_resources.py
+++ oversea/mechanics/factions/schemas/base_resources.py
@@ -1,9 +1,14 @@
-from pydantic import BaseModel
+from typing import Annotated
+
+from pydantic import BaseModel, Field
 
 
 class BaseResources(BaseModel):
-    cash: int = 0
-    geist: int = 0
-    intrigue_cards: int = 0
-    magic_cards: int = 0
-    goal_cards: int = 0
+    cash: Annotated[int, Field(ge=0)] = 0
+    geist: Annotated[int, Field(ge=0)] = 0
+    intrigue_cards: Annotated[int, Field(ge=0)] = 0
+    magic_cards: Annotated[int, Field(ge=0)] = 0
+    goal_cards: Annotated[int, Field(ge=0)] = 0
+
+    class Config:
+        frozen = True
