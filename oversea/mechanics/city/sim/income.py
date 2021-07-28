0abc545d4b3e9242e7334e5bb238cddee4579b2b
michal bargiel <michal5bargiel@gmail.com>
2021-07-28 18:55:19 +0200

    ✨ Applying effects.

--- oversea/mechanics/city/sim/income.py
+++ oversea/mechanics/city/sim/income.py
@@ -12,13 +12,21 @@ def sim(
     bank: Bank,
     income: Income,
     costs: list[list[Cost]],
+    effects: list[list[Income]],
     turns: int,
 ) -> Bank:
     for turn in range(turns):
         this_turn_costs = get_costs(costs, turn)
         for cost in this_turn_costs:
             bank -= cost
+        this_turn_effects = get_costs(effects, turn)
+        for effect in this_turn_effects:
+            income = income + effect
         bank += income
+        logging.log(
+            logging.DEBUG,
+            f'Turn {turn} income {json.dumps(income.dict())}'
+        )
         logging.log(
             logging.DEBUG,
             f"Turn {turn} ends with {json.dumps(bank.dict())}",
@@ -55,10 +63,14 @@ def arhant_with_buildings(turns: int) -> Bank:
         [buildings.eternal_forges.cost],
         [buildings.ash_oracle.cost],
     ]
+    effects = [
+        [Income(cash=2), Income(geist=1)]
+    ]
     return sim(
         bank=turn_0,
         income=income,
         costs=costs,
+        effects=effects,
         turns=turns,
     )
 
--- oversea/mechanics/factions/schemas/income.py
+++ oversea/mechanics/factions/schemas/income.py
@@ -2,4 +2,15 @@ from oversea.mechanics.factions.schemas.base_resources import BaseResources
 
 
 class Income(BaseResources):
-    pass
+    def __add__(
+        self,
+        other: "Income",
+    ) -> "Income":
+        this_dict = self.dict()
+        other_dict = other.dict()
+
+        increased_values = {
+            key: value + other_dict[key] for key, value in this_dict.items()
+        }
+
+        return Income(**increased_values)
