3874ffbfb2dc5573ae3d538b3f0c2a3b4575471c
michal bargiel <michal5bargiel@gmail.com>
2021-07-28 19:35:19 +0200

    ♻️ Use actions instead of costs and effects directly.

--- oversea/mechanics/city/sim/income.py
+++ oversea/mechanics/city/sim/income.py
@@ -4,27 +4,29 @@ import logging
 from oversea.mechanics.city.sim.safe_get import safe_get
 from oversea.mechanics.factions.arhant import buildings
 from oversea.mechanics.factions.arhant.faction import arhant
+from oversea.mechanics.factions.schemas.action import CreateBuilding, Action
 from oversea.mechanics.factions.schemas.bank import Bank
-from oversea.mechanics.factions.schemas.cost import Cost
 from oversea.mechanics.factions.schemas.income import Income
 
 
 def sim(
     bank: Bank,
     income: Income,
-    costs: list[list[Cost]],
-    effects: list[list[Income]],
+    actions: list[list[Action]],
     turns: int,
 ) -> Bank:
     for turn in range(turns):
-        this_turn_costs = safe_get(costs, turn)
-        for cost in this_turn_costs:
-            bank -= cost
-        this_turn_effects = safe_get(effects, turn)
-        for effect in this_turn_effects:
-            income = income + effect
+        this_turn_actions = safe_get(actions, turn)
+        for action in this_turn_actions:
+            if isinstance(action, CreateBuilding):
+                bank -= action.target.cost
+                income += action.target.effect
+
         bank += income
-        logging.log(logging.DEBUG, f"Turn {turn} income {json.dumps(income.dict())}")
+        logging.log(
+            logging.DEBUG,
+            f"Turn {turn} income {json.dumps(income.dict())}",
+        )
         logging.log(
             logging.DEBUG,
             f"Turn {turn} ends with {json.dumps(bank.dict())}",
@@ -50,16 +52,14 @@ def arhant_with_buildings(turns: int) -> Bank:
     turn_0 = arhant.bank + arhant.base_resources
 
     income = Income(**arhant.base_income.dict())
-    costs = [
-        [buildings.eternal_forges.cost],
-        [buildings.ash_oracle.cost],
+    actions = [
+        [CreateBuilding(target=buildings.eternal_forges)],
+        [CreateBuilding(target=buildings.ash_oracle)],
     ]
-    effects = [[Income(cash=2), Income(geist=1)]]
     return sim(
         bank=turn_0,
         income=income,
-        costs=costs,
-        effects=effects,
+        actions=actions,
         turns=turns,
     )
 
