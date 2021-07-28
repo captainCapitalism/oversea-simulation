26bc1081549d6dc08514ca12c7f6334e4daa5b19
michal bargiel <michal5bargiel@gmail.com>
2021-07-28 10:47:42 +0200

    ✨ Applying costs.

--- oversea/mechanics/city/sim/income.py
+++ oversea/mechanics/city/sim/income.py
@@ -1,6 +1,7 @@
 import json
 import logging
 
+from oversea.mechanics.factions.arhant import buildings
 from oversea.mechanics.factions.arhant.faction import arhant
 from oversea.mechanics.factions.schemas.bank import Bank
 from oversea.mechanics.factions.schemas.cost import Cost
@@ -14,6 +15,9 @@ def sim(
     turns: int,
 ) -> Bank:
     for turn in range(turns):
+        this_turn_costs = get_costs(costs, turn)
+        for cost in this_turn_costs:
+            bank -= cost
         bank += income
         logging.log(
             logging.DEBUG,
@@ -43,6 +47,22 @@ def base_arhant(turns: int) -> Bank:
     )
 
 
+def arhant_with_buildings(turns: int) -> Bank:
+    turn_0 = arhant.bank + arhant.base_resources
+
+    income = Income(**arhant.base_income.dict())
+    costs = [
+        [buildings.eternal_forges.cost],
+        [buildings.ash_oracle.cost],
+    ]
+    return sim(
+        bank=turn_0,
+        income=income,
+        costs=costs,
+        turns=turns,
+    )
+
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-    base_arhant(10)
+    arhant_with_buildings(10)
