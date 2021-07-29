0cf69b6a1e2c577b2ca56f4dd2d9428993eb3083
michal bargiel <michal5bargiel@gmail.com>
2021-07-29 17:30:04 +0200

    💄 Add some colours to messages.

--- oversea/cli/handlers.py
+++ oversea/cli/handlers.py
@@ -11,10 +11,13 @@ def new_simulation_structure(
     input_dir = "inputs"
     output_dir = "outputs"
     target_path = os.path.join(path, name)
+
+    coloured_name = typer.style(name, fg=typer.colors.RED)
+    coloured_path = typer.style(str(path), fg=typer.colors.BLUE)
     if os.path.exists(target_path):
-        typer.echo(f"Simulation {name} already exists.")
+        typer.echo(f"Simulation {coloured_name} already exists.")
     else:
         os.mkdir(target_path)
         os.mkdir(os.path.join(target_path, input_dir))
         os.mkdir(os.path.join(target_path, output_dir))
-        typer.echo(f"Simulation {name} created at {path}")
+        typer.echo(f"Simulation {coloured_name} created at {coloured_path}")
